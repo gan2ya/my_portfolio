ef2420cb3316557dd9282fb2f5fc47785c86c7ac
Hanna <48530596+gan2ya@users.noreply.github.com>
2020-01-15 11:15:05 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+ var list_proj = [
+   {"name": "Tribute page", "img": "https://codepen.io/Gan2ya/pen/mXJLwL/image/large.png", "href": "https://codepen.io/Gan2ya/full/mXJLwL", "info": "First project, dedicated personality of ukranian history.<p></p> Used: HTML Bootstrap"},
+                  
+                  {"name": 'Random Quote Machine', "img": "https://codepen.io/Gan2ya/pen/wmqjjw/image/large.png", "href": "https://codepen.io/Gan2ya/full/wmqjjw", "info": "Generation random quote.<p></p> Used: HTML CSS Bootstrap jQuery"},
+                  
+                  {"name": "Local Weather", "img": "https://codepen.io/Gan2ya/pen/XEZrYR/image/large.png", "href": "https://codepen.io/Gan2ya/full/XEZrYR", "info": "Show the local weather, used weather API.<p></p> Used: HTML CSS Bootstrap jQuery"},
+                  
+                  {"name": "Wikipedia Viewer", "img": "https://codepen.io/Gan2ya/pen/GxGEqG/image/large.png", "href": "https://codepen.io/Gan2ya/full/GxGEqG", "info": "Implementation Search viewer, aply wikipedia API.<p></p> Used: HTML CSS Bootstrap jQuery "},
+                                  
+                  {"name": "Tic Tac Toe Game", "img": "https://codepen.io/Gan2ya/pen/OJPjVyV/image/large.png", "href": "https://codepen.io/Gan2ya/full/OJPjVyV", "info": "Classial TicTacToeGame, wiсh includes different mode.<p></p> Used: HTML CSS JS"},
+   
+                  {"name": "Choropleth Map", "img": "https://codepen.io/Gan2ya/pen/qgGXNy/image/large.png", "href": "https://codepen.io/Gan2ya/full/qgGXNy", "info": "Choropleth Map<p></p>Used: HTML CSS D3.js "},
+   
+                  {"name": "Pomodoro Clock", "img":"https://codepen.io/Gan2ya/pen/QzvvdN/image/large.png", "href": "https://codepen.io/Gan2ya/full/QzvvdN", "info":"Flexible and easy Tomato Timer<p></p> Used: CSS React"},
+                  
+                  {"name": "Calculator", "img": "https://codepen.io/Gan2ya/pen/REVoad/image/large.png", "href": "https://codepen.io/Gan2ya/full/REVoad", "info": "Javascript Calculator<p></p>Used: CSS React"},
+   
+                  {"name": "Drum Machine", "img": "https://codepen.io/Gan2ya/pen/GwJgGm/image/large.png", "href": "https://codepen.io/Gan2ya/full/GwJgGm", "info": "First project for learning React<p></p> Used: CSS React"}]
+  
+ 
+let codepen_symbol= `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+width="100%" height="100%"
+viewBox="0 0 50 50" preserveAspectRatio="none"
+style=" fill:#ffffff;"><path d="M 25 4 L 4 17.34375 L 4 32.652344 L 25 46 L 46 32.65625 L 46 17.34375 Z M 25 29.183594 L 19.066406 25.070313 L 25 21.023438 L 30.933594 25.070313 Z M 27 17.605469 L 27 9.949219 L 40.429688 18.484375 L 34.410156 22.65625 Z M 23 17.605469 L 15.589844 22.65625 L 9.570313 18.484375 L 23 9.949219 Z M 12.09375 25.042969 L 8 27.832031 L 8 22.203125 Z M 15.570313 27.453125 L 23 32.605469 L 23 40.050781 L 9.589844 31.527344 Z M 27 32.605469 L 34.429688 27.453125 L 40.410156 31.527344 L 27 40.050781 Z M 37.90625 25.042969 L 42 22.203125 L 42 27.832031 Z"></path></svg>`
+
+let github_symbol = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+width="100%" height="100%"
+viewBox="0 0 24 24"
+style=" fill:#ffffff;">    <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path></svg>`
+
+let messager_symbol =`<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+width="100%" height="100%"
+viewBox="0 0 50 50" 
+style=" fill:#ffffff;"><path d="M 25 2 C 12.347656 2 2 11.597656 2 23.5 C 2 30.007813 5.132813 35.785156 10 39.71875 L 10 48.65625 L 11.46875 47.875 L 18.6875 44.125 C 20.703125 44.664063 22.800781 45 25 45 C 37.652344 45 48 35.402344 48 23.5 C 48 11.597656 37.652344 2 25 2 Z M 25 4 C 36.644531 4 46 12.757813 46 23.5 C 46 34.242188 36.644531 43 25 43 C 22.835938 43 20.742188 42.6875 18.78125 42.125 L 18.40625 42.03125 L 18.0625 42.21875 L 12 45.375 L 12 38.8125 L 11.625 38.53125 C 6.960938 34.941406 4 29.539063 4 23.5 C 4 12.757813 13.355469 4 25 4 Z M 22.71875 17.71875 L 10.6875 30.46875 L 21.5 24.40625 L 27.28125 30.59375 L 39.15625 17.71875 L 28.625 23.625 Z"></path></svg>`;
+
+   list_proj.forEach(function(val, ind){
+ $('<div></div>').addClass('proj').css({backgroundImage: 'url('+val.img+')'})
+   .append($('<div></div>').addClass('opacity'))
+   .append($('<div></div>').addClass('proj_name').text(val.name))
+   .append($('<div></div>').addClass('info').html(val.info))
+   .append($('<div></div>').addClass('link_project')
+          .append($('<a>').html(codepen_symbol).attr({href:val.href, target: "_blank"})))
+   .appendTo("#project");
+  });
+  
+document.getElementById("github-link").innerHTML+=github_symbol;
+document.getElementById("codepen-link").innerHTML+=codepen_symbol;
+document.getElementById("glitch-link").innerHTML+=messager_symbol;
+ 
+ 
